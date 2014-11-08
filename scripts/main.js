72722bb049c1b681204f0a1a179de036050deaba
Joseph Ahn <joseph.t.ahn@gmail.com>
2014-11-08 13:30:43 -0500

    add click listener

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,48 @@
 $(function() {
-  var canvas = document.getElementById("world");
-  var context = canvas.getContext("2d");
 
-  context.fillRect(0, 0, 512, 512);
+  // Constants
+  var cellSize = 16;
+  var canvasSize = 512;
 
-  for (var x = 0.5; x < 512; x += 16) {
-    context.moveTo(x, 0);
-    context.lineTo(x, 512);
-  }
+  // Draw canvas
+  var draw = function() {
+    var canvas = document.getElementById("world");
+    var context = window.context = canvas.getContext("2d");
 
-  for (var y = 0.5; y < 512; y += 16) {
-    context.moveTo(0, y);
-    context.lineTo(512, y);
-  }
+    context.fillRect(0, 0, canvasSize, canvasSize);
 
-  context.strokeStyle = '#474747';
-  context.stroke();
+    for (var x = 0.5; x < canvasSize; x += cellSize) {
+      context.moveTo(x, 0);
+      context.lineTo(x, canvasSize);
+    }
+
+    for (var y = 0.5; y < canvasSize; y += cellSize) {
+      context.moveTo(0, y);
+      context.lineTo(canvasSize, y);
+    }
+
+    context.strokeStyle = '#474747';
+    context.stroke();
+  };
+
+  // Fill in cell
+  var fill = function(x, y, color) {
+    context.fillStyle = color;
+    context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+  };
+
+  // Attach click listener
+  $('body').on('click', '#world', function(e) {
+
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+
+    var cellX = Math.floor(mouseX / cellSize);
+    var cellY = Math.floor(mouseY / cellSize);
+
+    fill(cellX, cellY, "#00CD00");
+  });
+
+  draw();
 
 });

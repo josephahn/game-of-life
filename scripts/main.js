325416b2e745414a67e3dc8de3a7de42517006c2
Joseph Ahn <joseph.t.ahn@gmail.com>
2014-11-09 13:54:28 -0500

    add matrix, unselect and clear functionality

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,24 @@ $(function() {
   // Constants
   var cellSize = 16;
   var canvasSize = 512;
+  var n = canvasSize / cellSize;
+
+  var matrix = [];
+  for (var i = 0; i < n; i++) {
+    var row = [];
+    for (var j = 0; j < n; j++) {
+      row.push(0);
+    }
+    matrix.push(row);
+  }
+
+  // Object to keep track of living cells using coordinate x and y
+  var alive = {};
 
   // Draw canvas
   var draw = function() {
     var canvas = document.getElementById("world");
+    // TODO: remove context from window
     var context = window.context = canvas.getContext("2d");
 
     context.fillRect(0, 0, canvasSize, canvasSize);
@@ -25,15 +39,59 @@ $(function() {
     context.stroke();
   };
 
+  // Converts coordinates into string to store as key in alive object
+  var coordinates = function(x, y) {
+    return y.toString() + x.toString();
+  };
+
   // Fill in cell
-  var fill = function(x, y, color) {
-    context.fillStyle = color;
-    context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+  var fill = function(x, y) {
+    console.log(' >>> filling ' + x + ' ' + y);
+    // Note: switch x and y when accessing elements in nested arrays
+    if (matrix[y][x]) {
+      console.log('here1');
+      context.fillStyle = '#000';
+      matrix[y][x] = 0;
+      delete alive[coordinates(x, y)];
+    } else {
+      console.log('here2');
+      context.fillStyle = '#00CD00';
+      matrix[y][x] = 1;
+      alive[coordinates(x, y)] = 1;
+    }
+    context.fillRect(x * cellSize + 1, y * cellSize + 1, cellSize - 1, cellSize - 1);
+
+    console.log(alive);
+
+  };
+
+  // Count a cell's living neighbors
+  var countNeighbors = function(x, y) {
+    // check all neighboring cells assuming they are in bounds
+    // return count
+    var count = 0;
+  };
+
+  // Calculate next generation
+  var nextGeneration = function() {
+    var next = [];
+    // loop through current alive
+    // countNeighbors
+    // if alive / dead... follow rules of game
+    return next;
+
   };
 
   // Start animation
   var start = function() {
     console.log('start');
+    // loop through all cells
+    // calculate next generation board
+
+    //alive = nextGeneration();
+    //clear();
+    //draw();
+    //loop through alive & fill cells
   };
 
   // Stop animation
@@ -44,6 +102,11 @@ $(function() {
   // Clear canvas
   var clear = function() {
     console.log('clear');
+    context.clearRect(0, 0, canvasSize, canvasSize);
+    // return to default style
+    context.fillStyle = '#000';
+    draw();
+    alive = [];
   };
 
   // Attach click listeners
@@ -55,7 +118,7 @@ $(function() {
     var cellX = Math.floor(mouseX / cellSize);
     var cellY = Math.floor(mouseY / cellSize);
 
-    fill(cellX, cellY, "#00CD00");
+    fill(cellX, cellY);
   });
 
   $('body').on('click', 'button[name=start]', function() {
